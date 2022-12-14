cecdcd4c2aef7ee46201fa340a058978d349da54
¨alcides.souza¨ <alcides.souza@bemobi.com>
2022-12-14 12:21:17 -0300

    ci: Test diferent secret

--- backend/game/infra/database/card_repository_test.go
+++ backend/game/infra/database/card_repository_test.go
@@ -25,31 +25,34 @@ func (suite *CardRepositoryTestSuite) SetupSuite() {
 		panic(err)
 	}
 
-	dbUser, ok := os.LookupEnv("DB_USER")
+	// dbUser, ok := os.LookupEnv("DB_USER")
+	// suite.True(ok)
+
+	// dbPass, ok := os.LookupEnv("DB_PASSWORD")
+	// suite.True(ok)
+
+	// dbHost, ok := os.LookupEnv("DB_HOST")
+	// suite.True(ok)
+
+	// dbPort, ok := os.LookupEnv("DB_PORT")
+	// suite.True(ok)
+
+	// dbName, ok := os.LookupEnv("DB_NAME")
+	// suite.True(ok)
+
+	dbUrl, ok := os.LookupEnv("DB_URL")
 	suite.True(ok)
 
-	dbPass, ok := os.LookupEnv("DB_PASSWORD")
-	suite.True(ok)
-
-	dbHost, ok := os.LookupEnv("DB_HOST")
-	suite.True(ok)
-
-	dbPort, ok := os.LookupEnv("DB_PORT")
-	suite.True(ok)
-
-	dbName, ok := os.LookupEnv("DB_NAME")
-	suite.True(ok)
-
-	conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		dbUser,
-		dbPass,
-		dbHost,
-		dbPort,
-		dbName,
-	)
+	// conn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+	// 	dbUser,
+	// 	dbPass,
+	// 	dbHost,
+	// 	dbPort,
+	// 	dbName,
+	// )
 
 	suite.ctx = context.Background()
-	db, err := pgx.Connect(suite.ctx, conn)
+	db, err := pgx.Connect(suite.ctx, dbUrl)
 	suite.NoError(err)
 	suite.Db = db
 }
