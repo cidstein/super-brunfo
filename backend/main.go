da1ccc87c6e5eef69504e0931417f1bf23094ab5
alcides.souza <alcides.souza@bemobi.com>
2022-12-17 11:01:01 -0300

    build: Changes for aws

--- backend/main.go
+++ backend/main.go
@@ -9,18 +9,22 @@ import (
 	"github.com/cidstein/super-brunfo/internal/api"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 	ctx := context.Background()
 
+	log.Print("Starting Super Brunfo! (backend) ...")
+
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
-		panic(err)
+		// panic(err)
 	}
 
 	dbUrl := os.Getenv("DB_URL")
+	log.Printf("DB_URL: %s", dbUrl)
 	db, err := pgx.Connect(ctx, dbUrl)
 	if err != nil {
 		fmt.Println("Error connecting to database")
@@ -28,12 +32,18 @@ func main() {
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Super Brunfo!"))
 	})
-
 	http.HandleFunc("/start", api.StartMatch(db))
 	http.HandleFunc("/play", api.PlayGame(db))
 	http.HandleFunc("/listcards", api.ListCards(db))
 
-	http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe("0.0.0.0:"+os.Getenv("APP_PORT"), nil)
+	if err != nil {
+		fmt.Println("Error starting server")
+		panic(err)
+	}
+
 }
